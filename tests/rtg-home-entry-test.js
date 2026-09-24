8f71ff071e2e828010818d88cd2557460b4516f5
ferro0403 <ferrettialessandro04@gmail.com>
2026-09-24 13:28:33 +0200

    test(RTG): cover Home Album shortcut

--- tests/rtg-home-entry-test.js
+++ tests/rtg-home-entry-test.js
@@ -8,6 +8,7 @@ assert.match(view,/data-rtg-home-open="run"/);
 assert.match(view,/data-rtg-home-open="squad"/);
 assert.doesNotMatch(view,/data-rtg-home-open="catalog"/);
 assert.match(view,/data-rtg-home-open="vending"/);
+assert.match(view,/data-rtg-home-open="album"/);
 assert.match(controller,/bindHomePager/);
 assert.match(controller,/deps\.renderRoadToGlory\(\{ destination:/);
 assert.doesNotMatch(view,/id:\s*"open-rtg-home"/);
